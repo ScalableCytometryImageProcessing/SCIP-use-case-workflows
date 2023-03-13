0f983d48d915bfa118acb8bedba2d30ca64c11d6
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2023-03-13 11:29:43 +0100

    Run on independent test sample, n_estimators resource

--- workflow/scripts/WBC/xgb_parameter_search.py
+++ workflow/scripts/WBC/xgb_parameter_search.py
@@ -1,5 +1,7 @@
-from pathlib import Path
 import pickle
+import tempfile
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 
 import numpy
 import pyarrow.parquet as pq
@@ -8,7 +10,7 @@ from xgboost import XGBClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import (
-    StratifiedKFold, cross_validate, HalvingRandomSearchCV, RandomizedSearchCV)
+    StratifiedKFold, cross_validate, HalvingRandomSearchCV, RandomizedSearchCV, PredefinedSplit)
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import make_pipeline
@@ -57,11 +59,11 @@ else:
         raise ValueError(snakemake.wildcards["mask"])
 
 if snakemake.wildcards["model"] == 'true':
-    model = DummyClassifier(strategy="uniform", random_state=0)
+    steps = [DummyClassifier(strategy="uniform", random_state=0)]
     param_distributions = {}
     resource = 'n_samples'
 else:
-    model = make_pipeline(
+    steps = [
         RandomUnderSampler(sampling_strategy="majority", random_state=0),
         RandomOverSampler(sampling_strategy="not majority", random_state=0),
         XGBClassifier(
@@ -71,59 +73,68 @@ else:
             tree_method="gpu_hist",
             use_label_encoder=False,
             random_state=0,
-            n_jobs=1
+            n_jobs=1,
+            gpu_id=snakemake.config["gpu_id"]
         )
-    )
+    ]
     param_distributions = {
         "xgbclassifier__max_depth": [7, 6, 5, 4, 3, 2],
         "xgbclassifier__learning_rate": [0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01, 0.001],
         "xgbclassifier__subsample": numpy.arange(start=0.1, stop=1.1, step=.1),
         "xgbclassifier__colsample_bytree": numpy.arange(start=0.1, stop=1.1, step=.1),
         "xgbclassifier__gamma": numpy.arange(start=0, stop=31, step=2),
-        "xgbclassifier__min_child_weight": numpy.arange(start=1, stop=32, step=2),
-        "xgbclassifier__n_estimators": numpy.arange(start=10, stop=301, step=10)
+        "xgbclassifier__min_child_weight": numpy.arange(start=1, stop=32, step=2)
+        # "xgbclassifier__n_estimators": numpy.arange(start=10, stop=301, step=10)
     }
-    resource = 'n_samples'
+    resource = 'xgbclassifier__n_estimators'
 
-if snakemake.wildcards["grid"] == "random":
-    grid = RandomizedSearchCV(
-        estimator=model,
-        param_distributions=param_distributions,
-        n_iter=int(snakemake.config["n"]),
-        refit=True,
-        n_jobs=snakemake.threads,
-        cv=5,
-        scoring='balanced_accuracy',
-        verbose=2,
-        return_train_score=True,
-        random_state=0
-
-    )
-else:
-    grid = HalvingRandomSearchCV(
-        estimator=model,
-        param_distributions=param_distributions,
-        factor=2,
-        resource=resource,
-        n_candidates=int(snakemake.config["n"]),
-        min_resources=5000,
-        aggressive_elimination=False,
-        refit=True,
-        n_jobs=snakemake.threads,
-        cv=5,
-        scoring='balanced_accuracy',
-        verbose=2,
-        return_train_score=True,
-        random_state=0
+with tempfile.TemporaryDirectory(dir="/srv/scratch/maximl/sklearn_cache") as tmp_path:
+    model = make_pipeline(
+        *steps,
+        memory=tmp_path
     )
 
-scores = cross_validate(
-    grid, Xs, y,
-    scoring=('balanced_accuracy', 'f1_macro', 'precision_macro', 'recall_macro'),
-    cv=StratifiedKFold(n_splits=5, shuffle=True, random_state=0),
-    return_train_score=True,
-    return_estimator=True
-)
+    if snakemake.wildcards["grid"] == "random":
+        grid = RandomizedSearchCV(
+            estimator=model,
+            param_distributions=param_distributions,
+            n_iter=int(snakemake.config["n"]),
+            refit=True,
+            n_jobs=snakemake.threads,
+            cv=5,
+            scoring='balanced_accuracy',
+            verbose=2,
+            return_train_score=True,
+            random_state=0
+
+        )
+    else:
+        grid = HalvingRandomSearchCV(
+            estimator=model,
+            param_distributions=param_distributions,
+            factor=2,
+            resource=resource,
+            n_candidates=int(snakemake.config["n"]),
+            min_resources=10,
+            max_resources=640,
+            aggressive_elimination=False,
+            refit=True,
+            n_jobs=snakemake.threads,
+            cv=5,
+            scoring='balanced_accuracy',
+            verbose=2,
+            return_train_score=True,
+            random_state=0
+        )
+
+    test_fold = df.index.get_level_values("meta_group").map(lambda x: 0 if x == 0 else -1).values
+    scores = cross_validate(
+        grid, Xs, y,
+        scoring=('balanced_accuracy', 'f1_macro', 'precision_macro', 'recall_macro'),
+        cv=PredefinedSplit(test_fold),
+        return_train_score=True,
+        return_estimator=True
+    )
 
 # STORE RESULTS
 
